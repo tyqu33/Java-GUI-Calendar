c0b43344b061af8f92faa6438e56c90bd3bf56c7
Yuyao Xu <yuyaoxu@yuyaodeair.mynetworksettings.com>
2025-10-24 18:20:11 -0400

    Implement the Event class

--- src/main/java/calendar/event/Event.java
+++ src/main/java/calendar/event/Event.java
@@ -1,36 +1,160 @@
 package calendar.event;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Objects;
 
 public class Event implements EventInterface {
+  private final String subject;
+  private final LocalDateTime start;
+  private final LocalDateTime end;
+  private final String description ;
+  private final String location;
+  private final String status;
+  private static final LocalTime START_TIME = LocalTime.of(8, 0);
+  private static final LocalTime END_TIME = LocalTime.of(17, 0);
+
+  private Event (EventBuilder builder) {
+    this.subject = builder.subject;
+    if (builder.isAllDayEvent && builder.end == null) {
+      this.start = builder.start.with(START_TIME);
+      this.end = builder.start.with(END_TIME);
+    } else {
+      this.start = builder.start;
+      this.end = builder.end;
+    }
+    this.description = builder.description;
+    this.location = builder.location;
+    this.status = builder.status;
+    if (this.end != null && this.end.isBefore(this.start)) {
+      throw new IllegalArgumentException("End date/time must be after start date/time.");
+    }
+  }
+
+  public static EventBuilder builder(String subject, LocalDateTime start) {
+    return new EventBuilder(subject, start);
+  }
+
+  public static class EventBuilder {
+    private String subject;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String description ;
+    private String location;
+    private String status = "public";
+    public boolean isAllDayEvent = false;
+
+    public EventBuilder(String subject, LocalDateTime start) {
+      if (subject == null || subject.trim().isEmpty()) {
+        throw new IllegalArgumentException("Subject cannot be null or empty");
+      }
+      if (start == null) {
+        throw new IllegalArgumentException("Start date cannot be null");
+      }
+      this.subject = subject.trim();
+      this.start = start;
+    }
+
+    public EventBuilder end(LocalDateTime end) {
+      this.end = end;
+      this.isAllDayEvent = false;
+      return this;
+    }
+
+    public EventBuilder description(String description) {
+      this.description = description;
+      return this;
+    }
+
+    public EventBuilder location(String location) {
+      this.location = location;
+      return this;
+    }
+
+    public EventBuilder status(String status) {
+      this.status = status;
+      return this;
+    }
+
+    public EventBuilder setAllDayEvent() {
+      this.isAllDayEvent = true;
+      return this;
+    }
+
+    public Event build() {
+      return new Event(this);
+    }
+  }
+
+  public boolean isAllDayEvent() {
+    if (end != null && start.toLocalTime().equals(START_TIME) &&
+        end.toLocalTime().equals(END_TIME)) {
+      return true;
+    }
+    return false;
+  }
 
   @Override
   public String getSubject() {
-    return "";
+    return subject;
   }
 
   @Override
   public LocalDateTime getStartDateTime() {
-    return null;
+    return start;
   }
 
   @Override
   public LocalDateTime getEndDateTime() {
-    return null;
+    return end;
   }
 
   @Override
   public String getDescription() {
-    return "";
+    return description;
   }
 
   @Override
   public String getLocation() {
-    return "";
+    return location;
   }
 
   @Override
   public String getEventStatus() {
-    return "";
+    return status;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(subject, start, end);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    Event event = (Event) obj;
+    return Objects.equals(subject, event.subject) && Objects.equals(start, event.start) && Objects.equals(end, event.end);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(subject);
+    sb.append(" from ");
+    sb.append(start);
+    if (end != null) {
+      sb.append(" to ");
+      sb.append(end);
+    }
+    if (location != null) {
+      sb.append(" at ");
+      sb.append(location);
+    }
+    return sb.toString();
   }
 }
