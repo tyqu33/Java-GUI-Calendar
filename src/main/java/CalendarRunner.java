b68b42f0801fdfd252ec24e995012859b51afc45
TingyuQu <qu.tin@northeastern.edu>
2025-10-28 00:10:43 -0400

    further abstraction of controller, decouple controller from specific input source

--- src/main/java/CalendarRunner.java
+++ src/main/java/CalendarRunner.java
@@ -1,6 +1,11 @@
 import calendar.controller.CalendarController;
 import calendar.model.Calendar;
 import calendar.view.CalendarView;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Program runner.
@@ -15,33 +20,81 @@ public class CalendarRunner {
   public static void main(String[] args) {
     Calendar calendarModel = new Calendar();
     CalendarView calendarView = new CalendarView();
-    CalendarController calendarController = new CalendarController(calendarModel, calendarView);
+    Readable input;
+    Appendable output = System.out;
 
-    if (args.length == 0 || !args[0].equals("--mode") || args.length < 2) {
-      System.out.println("Error: Wrong usage of running Calendar. Usage: java CalendarRunner --mode [interactive|headless] [command.txt]");
+    try {
+      input = verifyAndGetInput(args);
+      if (input == null) {
+        return;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
       return;
     }
+    //    if (args.length == 0 || !args[0].equals("--mode") || args.length < 2) {
+    //      System.out.println("Error: Wrong usage of running Calendar. Usage: java CalendarRunner --mode [interactive|headless] [command.txt]");
+    //      return;
+    //    }
+    //    String mode = args[1];
+    //    if (mode.equals("interactive")) {
+    //      // calendarController.runInteractiveMode();
+    //      input = new InputStreamReader(System.in);
+    //
+    //    } else if (mode.equals("headless")) {
+    //      if (args.length != 3) {
+    //        System.out.println("Error: Headless mode needs a third argument [command.txt].");
+    //        return;
+    //      }
+    //      String fileName = args[2];
+    //      // calendarController.runHeadlessMode(fileName);
+    //      try {
+    //        input = new FileReader(fileName);
+    //      } catch (FileNotFoundException e) {
+    //        throw new RuntimeException(e);
+    //      }
+    //    } else {
+    //      System.out.println("Error: Unknown mode " + mode);
+    //      return;
+    //    }
+    if (args[1].equals("interactive")) {
+      calendarView.displayWelcome();
+    }
+    CalendarController calendarController = new CalendarController(calendarModel, calendarView, input, output);
+    calendarController.go();
+    if (input instanceof java.io.FileReader) {
+      try {
+        ((FileReader) input).close();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+  }
+
+  private static Readable verifyAndGetInput(String[] args) throws FileNotFoundException {
+    if (args.length == 0 || !args[0].equals("--mode") || args.length < 2) {
+      System.out.println("Error: Wrong usage of running Calendar. Usage: java CalendarRunner --mode [interactive|headless] [command.txt]");
+      return null;
+    }
+
     String mode = args[1];
     if (mode.equals("interactive")) {
-      calendarController.runInteractiveMode();
+
+      // calendarController.runInteractiveMode();
+      return new InputStreamReader(System.in);
 
     } else if (mode.equals("headless")) {
       if (args.length != 3) {
         System.out.println("Error: Headless mode needs a third argument [command.txt].");
-        return;
+        return null;
       }
       String fileName = args[2];
-      calendarController.runHeadlessMode(fileName);
+      // calendarController.runHeadlessMode(fileName);
+      return new FileReader(fileName);
     } else {
       System.out.println("Error: Unknown mode " + mode);
-      return;
+      return null;
     }
-
-
-
-
-
-
-
   }
 }
--- src/main/java/calendar/controller/CalendarController.java
+++ src/main/java/calendar/controller/CalendarController.java
@@ -10,47 +10,69 @@ import java.util.Scanner;
 public class CalendarController implements CalendarControllerInterface{
   private final CalendarInterface calendar;
   private final CalendarView view;
-  public CalendarController(Calendar calendar, CalendarView calendarView) {
+  private final Readable input;
+  private final Appendable output;
+
+  public CalendarController(Calendar calendar, CalendarView calendarView, Readable input, Appendable output) {
     this.calendar = calendar;
     this.view = calendarView;
+    this.input = input;
+    this.output = output;
   }
-  public void runInteractiveMode() {
-    view.displayWelcome();
 
-    Scanner consoleScanner = new Scanner(System.in);
+  public void go() {
+    Scanner scanner = new Scanner(this.input);
     String commandLine;
-
-    while (true) {
-      System.out.print("> ");
-      commandLine = consoleScanner.nextLine();
-
+    boolean exitFlag = true;
+    while (scanner.hasNextLine() && exitFlag) { // java cant judge int as bool
+      commandLine = scanner.nextLine();
+      if (commandLine.trim().isEmpty()) {
+        continue;
+      }
       if (commandLine.equalsIgnoreCase("exit")) {
+        exitFlag = false;
         break;
       }
-
       processCommand(commandLine.trim());
     }
-    consoleScanner.close();
   }
-
-  public void runHeadlessMode(String fileName) {
-    File commandFile = new File(fileName);
-    try {
-      Scanner fileScanner = new Scanner(commandFile);
-      while (fileScanner.hasNextLine()) {
-        String commandLine = fileScanner.nextLine();
-        if (commandLine.equalsIgnoreCase("exit")) {
-          break;
-        }
-        if (commandLine.trim().isEmpty()) {
-          continue;
-        }
-        processCommand(commandLine.trim());
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("File not found");
-    }
-  }
+  //  public void runInteractiveMode() {
+  //    view.displayWelcome();
+  //
+  //    Scanner consoleScanner = new Scanner(System.in); // bad design! coupled with specific input source
+  //    String commandLine;
+  //
+  //    while (true) {
+  //      System.out.print("> ");
+  //      commandLine = consoleScanner.nextLine();
+  //
+  //      if (commandLine.equalsIgnoreCase("exit")) {
+  //        break;
+  //      }
+  //
+  //      processCommand(commandLine.trim());
+  //    }
+  //    consoleScanner.close();
+  //  }
+  //
+  //  public void runHeadlessMode(String fileName) {
+  //    File commandFile = new File(fileName);
+  //    try {
+  //      Scanner fileScanner = new Scanner(commandFile); // bad design! coupled with specific input source
+  //      while (fileScanner.hasNextLine()) {
+  //        String commandLine = fileScanner.nextLine();
+  //        if (commandLine.equalsIgnoreCase("exit")) {
+  //          break;
+  //        }
+  //        if (commandLine.trim().isEmpty()) {
+  //          continue;
+  //        }
+  //        processCommand(commandLine.trim());
+  //      }
+  //    } catch (FileNotFoundException e) {
+  //      System.out.println("File not found");
+  //    }
+  //  }
 
   private void processCommand(String commandLine) {
     String commandType = "";
